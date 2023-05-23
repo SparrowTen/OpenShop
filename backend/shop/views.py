86ab02f1aac6b71d3ceddba4d665eedcd7e37a16
Poyu <42506064+poyu39@users.noreply.github.com>
2023-05-23 21:39:58 +0800

    更改專案路徑

--- backend/shop/views.py
+++ backend/shop/views.py
@@ -12,10 +12,10 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request,
-                  'shop/product/list.html',
-                  {'category': category,
-                   'categories': categories,
-                   'products': products})
+                'shop/product/list.html',
+                {'category': category,
+                'categories': categories,
+                'products': products})
 
 
 def product_detail(request, product_id, slug):
@@ -25,6 +25,6 @@ def product_detail(request, product_id, slug):
                                 available=True)
     cart_product_form = CartAddProductForm()
     return render(request,
-                  'shop/product/detail.html',
-                  {'product': product,
-                   'cart_product_form': cart_product_form})
+                'shop/product/detail.html',
+                {'product': product,
+                'cart_product_form': cart_product_form})
